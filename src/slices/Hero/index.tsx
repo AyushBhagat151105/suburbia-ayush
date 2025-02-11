6b168cd46cd21d7f7c954617bb4bad9dbdfee55b
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-02-11 21:44:26 +0530

    Add Heading component and integrate it into Hero slice

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,6 +4,7 @@ import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import { PrismicNextLink } from "@prismicio/next";
 // min
 import { Bounded } from "@/components/Bounded";
+import { Heading } from "@/components/Heading";
 
 /**
  * Props for `Hero`.
@@ -18,9 +19,11 @@ const Hero: FC<HeroProps> = ({ slice }) => {
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="bg-brand-pink relative h-dvh overflow-hidden text-zinc-800 bg-texture"
     >
+      <Heading>
       <PrismicRichText field={slice.primary.heading} />
-
+      </Heading>
       <PrismicRichText field={slice.primary.body} />
       <PrismicNextLink field={slice.primary.buttone} />
     </Bounded>
